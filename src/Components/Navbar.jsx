15034b6f7485cb8468259398bca3e50db31f1e0d
Sudharsanan <sudharsanantv2005@gmail.com>
2024-10-24 19:50:04 +0530

    zuma

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,22 +6,30 @@ import logo from "../Components/images.png"
 
 function Navbar(){
     return(
-        <nav className="h-15 p-6 mt-8 mx-10 items-center text border rounded-lg">
-            
-            <div className="flex flex-shrink-0 items-center justify-between">
-                <div className="flex">
-                    <img className="h-7 w-8 rounded-full" src={logo} alt="" />
-                    <div className="font-Inter font-bold  text-xl mx-2">Portfolio</div>
-                </div>
+        <nav className="h-15 p-4 sm:p-6 mt-4 sm:mt-8 mx-4 sm:mx-10 items-center border rounded-lg">
+  <div className="flex flex-shrink-0 items-center justify-between">
+    <div className="flex items-center">
+      <img className="h-7 w-7 sm:h-8 sm:w-8 rounded-full" src={logo} alt="Logo" />
+      <div className="font-Inter font-bold text-lg sm:text-xl mx-2">Portfolio</div>
+    </div>
 
-                <div className="flex items-center gap-2 lg:gap-5">
-                    <div><a href="https://github.com/Sudharsanan-coder" target="blank"><FaGithub className="h-7 w-8"/></a></div>
-                    <div><a href="https://www.linkedin.com/in/sudharsanan-t-v-58459b257/" target="blank"><IoLogoLinkedin className="h-7 w-8"/></a></div>
-                    <div><a href="https://www.instagram.com/zudharzanan.xd/" target="blank"><FaInstagramSquare className="h-7 w-8"/></a></div>
-                    <div><a href="https://leetcode.com/u/T_V_Sudharsanan/" target="blank"><TbBrandLeetcode className="h-7 w-8"/></a></div>
-                </div>
-            </div>
-        </nav>
+    <div className="flex items-center gap-2 sm:gap-4 lg:gap-6">
+      <a href="https://github.com/Sudharsanan-coder" target="_blank" rel="noreferrer">
+        <FaGithub className="h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8" />
+      </a>
+      <a href="https://www.linkedin.com/in/sudharsanan-t-v-58459b257/" target="_blank" rel="noreferrer">
+        <IoLogoLinkedin className="h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8" />
+      </a>
+      <a href="https://www.instagram.com/zudharzanan.xd/" target="_blank" rel="noreferrer">
+        <FaInstagramSquare className="h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8" />
+      </a>
+      <a href="https://leetcode.com/u/T_V_Sudharsanan/" target="_blank" rel="noreferrer">
+        <TbBrandLeetcode className="h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8" />
+      </a>
+    </div>
+  </div>
+</nav>
+
     )
 }
 
